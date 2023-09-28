850f15050a6eaa305db6a07391e34818172821fe
jaaziel <180183@utags.com.mx>
2023-09-28 16:52:46 -0600

    API REST server books

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require('./config');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json());
+app.use(cors());
+ 
+app.get('/', function (req, res) {
+  res.status(200).json({
+    ok: true,
+    msg: '<h1> Bienvenido a mi servidor Rest </h1>'
+});
+});
+
+app.use(require('./routes/login'));
+app.use(require('./routes/usuario'));
+
+ mongoose.connect('mongodb+srv://admin:password_server20@cluster0.nckh7.mongodb.net/books', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true
+ }).then(() => console.log('Base de datos ONLINE')).catch(err => console.log(err));
+
+app.listen(process.env.PORT, () => {
+  console.log('El servidor esta en linea por el puerto', process.env.PORT);
+});
